36ca0c01e84fca13725357506466ac44ae960e5b
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-10-19 19:39:12 +0530

    1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <h1>Chai aur react with Vite | Pranav </h1>
+    <Chai/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>Chai is ready in Vite</h3>
+    )
+}
+
+export default Chai;
